c8482bef755226d026ec5af9c638b362cfff24b9
JohnOK <john.ok.esp@gmail.com>
2022-05-31 15:33:09 +0200

    Added python version of pad-array. Need to write spec file.

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,19 @@
 #REMEMBER TO PSEUDOCODE
+# 1. Check if list lenght is less than min_size.
+# 2. Check how much padding is needed. 
+# 3. Add padding to list. 
+# 4. Return list.
 def pad(array, min_size, value = None):
+    array_length = len(array)
+    padded_array = array
+    if len(array) >= min_size:
+        return array
+    
+    padding_amount = min_size - array_length
+
+    for i in range(padding_amount):
+        padded_array.append(value)
+    
+    return padded_array
+
+# print(pad([1,2,3], 5, 'apple'))
